ea4d29ee2b5f9a84b9ccf98b3e59fcd3ddf81550
Isac <isac_asj91@hotmail.com>
2017-10-16 20:04:26 -0200

    init

---
+++ README.md
@@ -0,0 +1,16 @@
+# Desafio - Github
+
+### Install dependencies
+```
+yarn install or npm install
+```
+
+### Start Project
+```
+yarn start or npm start
+```
+
+### Open Project
+```
+https://localhost/8080
+```
---
+++ src/container/Main/index.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class Main extends Component {
+
+  render() {
+    return (
+      <main className="main">
+        <div className="container-fluid">
+          React Redux
+        </div>
+      </main>
+    )
+  }
+}
+
+export default Main
